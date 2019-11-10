daf6cc9ca3ceddce6e5a0f05d7e369c0f9f747ca
Milevskaia <o.milevkaya@gmail.com>
2019-11-10 23:56:41 +0200

    Milevska Olha add project

---
+++ Lab2_version2/forms/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms import validators
+
+from dao.db import PostgresDB
+from dao.orm.model import ormTest, ormQuestion
+
+db = PostgresDB()
+
+
+def test_choices():
+    return db.session.query(ormTest).all()
+
+
+def test_question_choices():
+    return db.session.query(ormQuestion).all()
+
+
+class TestForm(FlaskForm):
+    test_name = StringField("Name: ", [validators.DataRequired(), validators.Length(max=63)])
+    test_variant = IntegerField('Variant: ')
+    submit = SubmitField("Save")
+
+
+class QuestionForm(FlaskForm):
+    question_text = StringField("Name: ", [validators.DataRequired(), validators.Length(max=63)])
+    test_id = IntegerField()
+    submit = SubmitField("Save")
+
+
+class QuestionVariantForm(FlaskForm):
+    answer_variant_text = StringField("Name: ", [validators.DataRequired(), validators.Length(max=511)])
+    question_id = IntegerField()
